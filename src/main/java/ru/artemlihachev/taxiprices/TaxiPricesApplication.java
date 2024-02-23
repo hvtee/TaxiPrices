ac788c496a481eb3983983318feb7b30b3bd6e31
Artem <milkartem2@mail.ru>
2024-02-23 16:04:00 +0300

    init commit

---
+++ src/main/java/ru/artemlihachev/taxiprices/TaxiPricesApplication.java
@@ -0,0 +1,13 @@
+package ru.artemlihachev.taxiprices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiPricesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiPricesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/artemlihachev/taxiprices/TaxiPricesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.artemlihachev.taxiprices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiPricesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
